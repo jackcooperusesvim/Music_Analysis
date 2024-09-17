121a80cbd2e6da42b191ffa125579f1bad6ed1f3
jackcooperusesvim <andreft316@gmail.com>
2024-09-17 12:21:45 -0400

    better script indentation

--- TheirOldStuff.py
+++ TheirOldStuff.py
@@ -5,12 +5,20 @@ import sqldf
 df = pd.read_csv("ClassicHit.csv")
 
 artist_info = sqldf.run("""
-    SELECT artist, MIN(year) AS artist_start_year, MAX(year) AS artist_end_year, MAX(year)-MIN(year) AS artist_career_length 
-        FROM df GROUP BY artist""")
+    SELECT 
+            artist, 
+            MIN(year) AS artist_start_year, 
+            MAX(year) AS artist_end_year, 
+            MAX(year)-MIN(year) AS artist_career_length 
+
+    FROM df GROUP BY artist""")
 
 full_df = sqldf.run("""
-    SELECT *, (year-artist_start_year) AS years_since_debut 
-        FROM df LEFT JOIN artist_info 
+    SELECT 
+        *, 
+        (year-artist_start_year) AS years_since_debut 
+    FROM df 
+        LEFT JOIN artist_info 
             ON df.artist = artist_info.artist""")
 
 if full_df is not None:
