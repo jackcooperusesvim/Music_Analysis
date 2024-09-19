30b9878799b8fdc8dd4a4c6a30d23b07a8052560
jackcooperusesvim <andreft316@gmail.com>
2024-09-19 12:27:54 -0400

    only nearest neigbor

--- Model.py
+++ Model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn import tree, neighbors
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import  ConfusionMatrixDisplay, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score
 from icecream import ic
 import matplotlib.pyplot as plt
 from typing import Any
@@ -10,7 +10,7 @@ from numpy import average
 import numpy as np
 print('imports are over')
 
-random_state = 123456789
+random_state = 234
 data_section = pd.read_csv("ClassicHit2.csv")
 genres = ['Alt. Rock', 'Blues', 'Country', 'Disco', 'EDM', 'Folk', 'Funk',
        'Gospel', 'Jazz', 'Metal', 'Pop', 'Punk', 'R&B', 'Rap', 'Reggae',
@@ -21,18 +21,6 @@ genres = ['Alt. Rock', 'Blues', 'Country', 'Disco', 'EDM', 'Folk', 'Funk',
 #-------------------------------------------------------------------------------------------------------------
 # Functions
 #-------------------------------------------------------------------------------------------------------------
-def regress_to_class(model, in_x):
-    return [num.round() for num in model.predict(in_x)]
-
-def vote_prediction(km, tree, in_x):
-    predictions = pd.DataFrame()
-    predictions['km'] = km.predict(in_x)
-    predictions['tree'] = tree.predict(in_x)
-    return predictions.apply(vote,axis=1)
-
-def vote(pred: pd.Series):
-    return round(average(pred))
-
 def encode_genres(In: pd.Series):
     return genres.index(In.Genre)
 
@@ -65,9 +53,6 @@ second_model_x_columns: list[str] =  first_model_x_columns+first_model_y_columns
 
 second_model_y_columns: list[str] = ["Popularity"]
 
-tree_model = tree.DecisionTreeRegressor()
-knn_model = neighbors.KNeighborsRegressor()
-
 train_x = train[["Year","Duration","Danceability","Energy","Key","Loudness","Mode","Speechiness","Acousticness","Instrumentalness","Liveness","Valence","Tempo","years_since_debut"]]
 train_y = train["pop_int"]
 val_x = val[["Year","Duration","Danceability","Energy","Key","Loudness","Mode","Speechiness","Acousticness","Instrumentalness","Liveness","Valence","Tempo","years_since_debut"]]
@@ -75,22 +60,32 @@ val_y = val["pop_int"]
 test_x = test[["Year","Duration","Danceability","Energy","Key","Loudness","Mode","Speechiness","Acousticness","Instrumentalness","Liveness","Valence","Tempo","years_since_debut"]]
 test_y = test["pop_int"]
 
-tree_model.fit(train_x,train_y)
-knn_model.fit(train_x,train_y)
 
-data_tup = ((train_x,train_y),(val_x,val_y),(test_x,test_y))
-ic(data_tup)
-data_names = ('training','validation','testing')
-for x,y in data_tup:
-    tree_acc = accuracy_score(y_true=y,y_pred=regress_to_class(tree_model,x))
-    knn_acc = accuracy_score(y_true=y,y_pred=regress_to_class(knn_model,x))
-    combo_acc = accuracy_score(y_true=y,y_pred=vote_prediction(knn_model,tree_model,x))
-    ic(f'accuracy  k-means: {knn_acc}')
-    ic(f'accuracy tree: {tree_acc}')
-    ic(f'accuracy combo: {combo_acc}')
-    ConfusionMatrixDisplay.from_predictions(y_true=y,y_pred = regress_to_class(knn_model,x))
-    ConfusionMatrixDisplay.from_predictions(y_true=y,y_pred = regress_to_class(tree_model,x))
-    ConfusionMatrixDisplay.from_predictions(y_true=y,y_pred = vote_prediction(knn_model,tree_model,x))
-    plt.show()
+data_tup = ((train_x,train_y),
+            (val_x,val_y),
+            # (test_x,test_y)
+            )
+acc_list = []
+for i in range(0,10,2):
+    n_neighbors = i+1
+    knn_model = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors)
+    knn_model.fit(train_x,train_y)
 
+    print(f'number of neighbors: {n_neighbors}')
+    data_names = ('training','validation','testing')
+    counter = 0
+    training = True
+    for x,y in data_tup:
+        print(data_names[counter])
+        counter+=1
+        knn_acc = accuracy_score(y_true=y,y_pred=knn_model.predict(x))
+        if not training:
+            acc_list.append(knn_acc)
+
+        print(f'\taccuracy  k-means: {knn_acc}')
+        print(confusion_matrix(y_true=y,y_pred = knn_model.predict(x)))
+        training = not training
+fig, ax = plt.subplots()
+ax.plot(range(1,len(acc_list)+1),acc_list)
+plt.show()
 
