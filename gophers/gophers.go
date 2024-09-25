0d8632c94fe7ffb2514463340a4acf5e3dd4c752
jackcooperusesvim <andreft316@gmail.com>
2024-09-25 16:33:13 -0400

    basic gophers working

--- gophers/gophers.go
+++ gophers/gophers.go
@@ -3,7 +3,6 @@ package gophers
 import (
 	"errors"
 	"fmt"
-	"math"
 )
 
 type Series []float64
@@ -12,40 +11,23 @@ type DataFrame struct {
 	shape Shape
 	err   error
 }
+
 type Shape struct {
 	cols int
 	rows int
 }
 
-type Knn struct {
-	train_x   DataFrame
-	train_y   DataFrame
-	in_shape  Shape
-	out_shape Shape
-	k         int
-}
+func SerArrToDf(dta []Series) DataFrame {
 
-type Model interface {
-	fit(x DataFrame, y DataFrame) (Model, error)
-	predict_regress(x DataFrame) (y DataFrame)
-	predict_classify(x DataFrame) (y DataFrame)
-}
-
-func SerArrToDf(dta []Series) (DataFrame, error) {
 	out := DataFrame{
 		data: dta,
 	}
-	_, err := out.CalcShape()
-	return out, err
-}
 
-func SeriesDistance(s1 Series, s2 Series) (out float64) {
-	//This is squared euclidean distance
-	for i, v := range s1 {
-		out += (v - s2[i]) * (v - s2[i])
-	}
+	out.shape, out.err = out.CalcShape()
+
 	return out
 }
+
 func (df *DataFrame) CalcShape() (Shape, error) {
 
 	shape := Shape{}
@@ -70,59 +52,3 @@ func (df *DataFrame) CalcShape() (Shape, error) {
 	shape.rows = sub_len
 	return shape, nil
 }
-func (knn *Knn) Distance(x DataFrame) (DataFrame, error) {
-	actual_input_shape, err := x.CalcShape()
-	if err != nil {
-		return DataFrame{}, err
-	}
-	if actual_input_shape != knn.in_shape {
-		return DataFrame{}, errors.New(fmt.Sprintf("Shape on incoming DataFrame (%d,%d) does not match proper input shape (%d,%d)", actual_input_shape.rows, actual_input_shape.cols, knn.in_shape.cols, knn.in_shape.rows))
-	}
-
-	out_data := make([]Series, knn.in_shape.rows)
-
-	for i, series_train := range knn.train_x.data {
-		pred_distances := make(Series, knn.in_shape.rows)
-		for j, series_pred := range x.data {
-			pred_distances[j] = SeriesDistance(series_train, series_pred)
-		}
-		out_data[i] = pred_distances
-	}
-
-	out, err := SerArrToDf(out_data)
-
-	if err != nil {
-		return DataFrame{}, err
-	}
-	return out, nil
-}
-
-func Build_Knn(k int) Knn {
-	return Knn{k: k}
-}
-
-// func (knn *Knn) predict_regress(x DataFrame) (y DataFrame) {
-// 	return
-// }
-
-// func (knn *Knn) predict_classify(x DataFrame) (y DataFrame) {
-// 	return
-// }
-
-// func (knn *Knn) fit(x DataFrame, y DataFrame) (err error) {
-//
-// 	knn.in_shape, err = x.CalcShape()
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	knn.out_shape, err = y.CalcShape()
-// 	if err != nil {
-// 		return err
-// 	}
-//
-// 	knn.train_x = x
-// 	knn.train_y = y
-//
-// 	return err
-// }
--- gophers/gophers_test.go
+++ gophers/gophers_test.go
@@ -23,35 +23,25 @@ func TestSerArrToDf(t *testing.T) {
 		},
 	}
 
-	df, err := SerArrToDf(sers)
-	if err != nil {
-		t.Errorf("Error creating DataFrame: %s", err)
+	df := SerArrToDf(sers)
+	if df.err != nil {
+		t.Errorf("Error creating DataFrame: %s", df.err)
 	}
 	if df.shape.cols != 3 {
-		t.Errorf("DataFrame has %d columns, not 3", df.shape.cols)
+		t.Errorf("DataFrame has %d columns, not 3 {%+v}", df.shape.cols, df)
 	}
 	if df.shape.rows != 3 {
-		t.Errorf("DataFrame has %d rows, not 3", df.shape.rows)
+		t.Errorf("DataFrame has %d rows, not 3 {%+v}", df.shape.rows, df)
 	}
 	for i, row := range df.data {
 		if len(row) != len(expected_df.data[i]) {
-			t.Errorf("DataFrame is not equal to expected DataFrame")
+			t.Errorf("DataFrame is not equal to expected DataFrame exp:{%+v}\n\nact:{%+v}", expected_df, df)
 		}
 		for j, v := range row {
 			if v != expected_df.data[i][j] {
-				t.Errorf("DataFrame is not equal to expected DataFrame")
+				t.Errorf("DataFrame is not equal to expected DataFrame exp:{%+v}\n\nact:{%+v}", expected_df, df)
 			}
 		}
-	}
-}
-
-func TestSeriesDistance(t *testing.T) {
-	s1 := []float64{1, 1, 5}
-	s2 := []float64{1, 3, 3}
-	expected := 16.0
-	actual := SeriesDistance(s1, s2)
-	if actual != expected {
-		t.Errorf("Distance between %v and %v is %f, not %f", s1, s2, actual, expected)
 	}
 }
 
@@ -61,52 +51,18 @@ func TestCalcShape(t *testing.T) {
 		[]float64{4, 5, 6},
 		[]float64{7, 8, 9},
 	}
-	df, err := SerArrToDf(sers)
-	if err != nil {
-		t.Errorf("Error creating DataFrame: %s", err)
+	df := SerArrToDf(sers)
+	if df.err != nil {
+		t.Errorf("Error creating DataFrame: %s", df.err)
 	}
-	shape, err := df.CalcShape()
-	if err != nil {
-		t.Errorf("Error calculating shape: %s", err)
+	df.CalcShape()
+	if df.err != nil {
+		t.Errorf("Error calculating shape: %s", df.err)
 	}
-	if shape.cols != 3 {
-		t.Errorf("DataFrame has %d columns, not 3", shape.cols)
+	if df.shape.cols != 3 {
+		t.Errorf("DataFrame has %d columns, not 3", df.shape.cols)
 	}
-	if shape.rows != 3 {
-		t.Errorf("DataFrame has %d rows, not 3", shape.rows)
-	}
-}
-
-func TestKnnDistance(t *testing.T) {
-	knn := Build_Knn(2)
-
-	x_df, err := SerArrToDf(
-		[]Series{
-			[]float64{1, 1, 1},
-			[]float64{2, 2, 2},
-			[]float64{3, 3, 3},
-		})
-	dist_df, err := SerArrToDf(
-		[]Series{
-			[]float64{1, 1, 1},
-			[]float64{2, 2, 2},
-			[]float64{3, 3, 3},
-		})
-	exp_dist_df, err := SerArrToDf(
-		[]Series{
-			[]float64{0, 3, 0},
-			[]float64{0, 0, 0},
-			[]float64{0, 0, 0},
-		})
-	if err != nil {
-		t.Errorf("Error creating DataFrame: %s", err)
-	}
-
-	actual_dist_df, err := knn.Distance(x_df)
-	if err != nil {
-		t.Errorf("Error calculating distance: %s", err)
-	}
-	if exp_dist_df.shape != actual_dist_df.shape {
-		t.Errorf("Distance DataFrame has %d columns, not %d", actual_dist_df.shape.cols, exp_dist_df.shape.cols)
+	if df.shape.rows != 3 {
+		t.Errorf("DataFrame has %d rows, not 3", df.shape.rows)
 	}
 }
